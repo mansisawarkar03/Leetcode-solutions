9f2ce5645be834d2653f0d02c029c3eff5d4b128
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-07-20 20:12:46 +0530

    Time: 3 ms (96.23%) | Memory: 51.9 MB (88.99%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,22 +1,21 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
+        int l=0;
+        int zeros=0;
         int maxlen=0;
 
-        for(int i=0;i<nums.length;i++)
+        for(int r=0;r<nums.length;r++)
         {
-            int zeros=0;
-            for(int j=i;j<nums.length;j++)
-            {
-                if(nums[j]==0) zeros++;
+            if(nums[r]==0) zeros++;
 
-                if(zeros<=k)
-                {
-                    maxlen=Math.max(maxlen,j-i+1);
-                }
-                else
-                    break;
+            while(zeros>k)
+            {
+                if(nums[l]==0) zeros--;
+                l++;
             }
+
+            maxlen=Math.max(maxlen,r-l+1);
         }
         return maxlen;
     }
