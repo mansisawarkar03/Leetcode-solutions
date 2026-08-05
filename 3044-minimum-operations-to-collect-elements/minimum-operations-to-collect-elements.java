14335db1ce0c69ac6d96bee1de8ff9295951c7a2
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-08-05 14:29:59 +0530

    Time: 2 ms (27.61%) | Memory: 44 MB (33.80%) - LeetSync

---
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minOperations(List<Integer> nums, int k) {
+
+        List<Integer> list=new ArrayList<>();
+        int ans=0;
+
+        for(int i=1;i<=k;i++)
+        {
+            list.add(i);
+        }
+
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(list.isEmpty())
+                return ans;
+            if(list.contains(nums.get(i)))
+            {
+                list.remove(nums.get(i));
+                ans++;
+            }
+            else
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+        
+    }
+}
