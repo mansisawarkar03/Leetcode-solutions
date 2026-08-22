8e9f7da996c535a310f18bb3f9c7ce17c6abcdeb
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-08-23 00:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (85.15%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -16,27 +16,47 @@ class Solution {
             return null;
         }
 
-        HashSet<Integer> set=new HashSet<>();
+        // HashSet<Integer> set=new HashSet<>();
 
-        ListNode h=head;
-        while(h!=null)
+        // ListNode h=head;
+        // while(h!=null)
+        // {
+        //     set.add(h.val);
+        //     h=h.next;
+        // }
+
+        // ArrayList<Integer> list=new ArrayList<>(set);
+        // Collections.sort(list);
+        // ListNode l=new ListNode(list.get(0));
+        // ListNode m=l;
+        // for(int i=1;i<set.size();i++)
+        // {
+        //     l.next=new ListNode(list.get(i));
+        //     l=l.next;
+
+        // }
+        // l.next=null;
+
+        // return m;
+
+        
+        ListNode cur=head;
+
+        while(cur!=null && cur.next!=null)
         {
-            set.add(h.val);
-            h=h.next;
+            if(cur.val==cur.next.val)
+            {
+                cur.next=cur.next.next;
+            }
+            else
+            {
+                
+                cur=cur.next;
+            }
         }
 
-        ArrayList<Integer> list=new ArrayList<>(set);
-        Collections.sort(list);
-        ListNode l=new ListNode(list.get(0));
-        ListNode m=l;
-        for(int i=1;i<set.size();i++)
-        {
-            l.next=new ListNode(list.get(i));
-            l=l.next;
-
-        }
-        l.next=null;
-
-        return m;
+        return head;
     }
+
+
 }
