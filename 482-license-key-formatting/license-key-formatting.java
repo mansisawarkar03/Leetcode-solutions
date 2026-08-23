2e851ca3b39597feb3f946a32fb18c2d845e5c38
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-08-23 18:44:54 +0530

    Time: 14 ms (37.83%) | Memory: 46.5 MB (45.97%) - LeetSync

---
+++ 482-license-key-formatting/license-key-formatting.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char ch=s.charAt(i);
+
+            if(ch!='-')
+            {
+                sb.append(ch);
+                count++;
+            }
+            if(count==k)
+            {
+                sb.append("-");
+                count=0;
+            }
+        }
+
+        sb.reverse();
+
+        if(sb.length()>0 && sb.charAt(0)=='-')
+        {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString().toUpperCase();
+        
+    }
+}
