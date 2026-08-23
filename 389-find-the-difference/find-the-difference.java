d158569ef7586239383a05ac2d46947f0c77e5c9
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-08-23 18:24:46 +0530

    Time: 3 ms (41.70%) | Memory: 42.8 MB (93.19%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,28 +1,44 @@
 class Solution {
     public char findTheDifference(String s, String t) {
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // for(int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+
+        // for(int i = 0; i < t.length(); i++) {
+
+        //     char ch = t.charAt(i);
+
+        //     if(!map.containsKey(ch)) {
+        //         return ch;
+        //     }
+
+        //     map.put(ch, map.get(ch) - 1);
+
+        //     if(map.get(ch) == 0) {
+        //         map.remove(ch);
+        //     }
+        // }
+
+        // return ' ';
+
+
+        char ans=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            ans^=s.charAt(i);
         }
 
-        for(int i = 0; i < t.length(); i++) {
-
-            char ch = t.charAt(i);
-
-            if(!map.containsKey(ch)) {
-                return ch;
-            }
-
-            map.put(ch, map.get(ch) - 1);
-
-            if(map.get(ch) == 0) {
-                map.remove(ch);
-            }
+        for(int i=0;i<t.length();i++)
+        {
+            ans^=t.charAt(i);
         }
 
-        return ' ';
+        return ans;
+
     }
 }
