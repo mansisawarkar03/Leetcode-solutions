ccbef0e0e98cd9a930d83ff51e3d3295bb326b9e
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-09-01 18:52:06 +0530

    Time: 3 ms (25.74%) | Memory: 45 MB (9.45%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        int n=nums.length;
+        // int[] nums1=new int[n/2];
+        // int[] nums2=new int[n/2];
+
+        // Arrays.sort(nums);
+        // int k=0;
+        // for(int i=0;i<nums.length/2;i++)
+        // {
+        //     nums1[i]=nums[k++];
+        //     nums2[i]=nums[k++];
+
+        // }
+
+        // if(!Arrays.equals(nums1,nums2))
+        // {
+        //     return true;
+        // }
+        // return false;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer> entry: map.entrySet())
+        {
+            if(entry.getValue()>2)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
