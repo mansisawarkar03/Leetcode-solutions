9a54fb6f44b4ff49e720d8fcdb24bc75c7b039fc
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-09-25 13:12:53 +0530

    Time: 4 ms (44.38%) | Memory: 52.1 MB (54.37%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -9,7 +9,7 @@ class Solution {
         {
             if(nums[r]==0) zeros++;
 
-            if(zeros>k)
+            while(zeros>k)
             {
                 if(nums[l]==0) zeros--;
                 l++;
