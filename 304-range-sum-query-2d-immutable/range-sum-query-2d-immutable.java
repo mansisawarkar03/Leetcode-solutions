f425687e4b0067dd1247a50ae2e4a508f19f06f2
Mansi Prashant Sawarkar <mansisawarkar03@gmail.com>
2026-09-26 20:33:35 +0530

    Time: 109 ms (28.58%) | Memory: 144.5 MB (5.14%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,21 +1,37 @@
 class NumMatrix {
     int[][] matrix;
 
+    int[][] prefix;
+
     public NumMatrix(int[][] matrix) {
         this.matrix=matrix;
+
+        int m=matrix.length;
+        int n=matrix[0].length;
+        prefix=new int[m+1][n+1];
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                prefix[i][j]=matrix[i-1][j-1]+prefix[i][j-1]+prefix[i-1][j]-prefix[i-1][j-1];
+            }
+        }
 
         
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;i++)
-        {
-            for(int j=col1;j<=col2;j++)
-            {
-                ans+=matrix[i][j];
-            }
-        }
+        // int ans=0;
+        // for(int i=row1;i<=row2;i++)
+        // {
+        //     for(int j=col1;j<=col2;j++)
+        //     {
+        //         ans+=matrix[i][j];
+        //     }
+        // }
+        // return ans;
+
+        int ans=prefix[row2+1][col2+1]-prefix[row1][col2+1]-prefix[row2+1][col1]+prefix[row1][col1];
         return ans;
         
     }
